7cffd3ba37239bef89a0dbc58866e53033ea22c8
Shishir Sharma <crmaccr53@gmail.com>
2022-03-09 11:37:36 +0545

    create grid with gap instead of cells with border

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const container = document.createElement("div");
+const container = document.createElement("div"); // overall container
 container.classList.add("container");
 
 const grid = document.createElement("div");
@@ -8,42 +8,53 @@ const resetButton = document.createElement("button");
 resetButton.classList.add("reset");
 resetButton.textContent = "reset";
 
-container.appendChild(resetButton);
-
-createAndDisplayGrid(16);
-
-function setupListenersForGridCells() {
-  const gridSquares = grid.querySelectorAll("div");
-  gridSquares.forEach(function (grid) {
-    grid.addEventListener("mouseenter", function () {
-      console.log("hovered over me");
-      this.style.backgroundColor = "yellow";
-    });
-  });
-}
-
 resetButton.addEventListener("click", function () {
   let newGridSize = 0;
   do {
     newGridSize = +prompt("enter new Grid Size (< 100)", "25");
   } while (newGridSize > 100);
   if (newGridSize) {
-    createAndDisplayGrid(newGridSize);
+    displayGrid(newGridSize);
   }
 });
 
-function createAndDisplayGrid(size) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  const squareSize = 600 / size;
-  square.style.cssText = `height:${squareSize}px; width:${squareSize}px;`;
-  grid.innerHTML = "";
-  for (let i = 1; i <= size; i++) {
-    for (let j = 1; j <= size; j++) {
-      grid.appendChild(square.cloneNode());
-    }
-  }
-  setupListenersForGridCells();
-}
+displayGrid(16);
+
+container.appendChild(resetButton);
 container.appendChild(grid);
 document.body.appendChild(container);
+
+function createGridCells(size) {
+  const cells = [];
+  const square = document.createElement("div");
+  square.classList.add("square");
+  const squareSize = Math.floor(600 / (size + 1)); // To allow space for gap in the grid
+  square.style.cssText = `height:${squareSize}px; width:${squareSize}px;`;
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+      cells.push(square.cloneNode());
+    }
+  }
+  setupListenersForGridCells(cells);
+  return cells;
+}
+
+function setupListenersForGridCells(cells) {
+  cells.forEach(function (cell) {
+    cell.addEventListener("mouseenter", function () {
+      this.style.backgroundColor = "yellow";
+    });
+  });
+}
+
+function addCellsToGrid(cells, cellGrid = grid) {
+  cellGrid.innerHTML = "";
+  cells.forEach(function (cell) {
+    cellGrid.appendChild(cell);
+  });
+}
+
+function displayGrid(size) {
+  const cells = createGridCells(size);
+  addCellsToGrid(cells);
+}
